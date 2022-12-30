3bdaedeace26e91afa9fc86eb6b98ebb33e3fcd0
JiahuiKang <kangjiahui2003@163.com>
2022-12-31 02:39:31 +0800

    Site updated: 2022-12-31 02:39:30

--- js/my.js
+++ js/my.js
@@ -7,7 +7,7 @@ var app = document.getElementById('footer');
 app.removeAttribute('id')
 
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://wimg.588ku.com/gif620/20/07/13/c3d2ccdfdbf09fe4472a874be0ba5137.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='img.zcool.cn/community/01ea4b5c091dbea80120925222b559.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 
